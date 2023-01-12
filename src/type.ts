d0fc503a50151e895f7ed38c48654554e9bad4a5
tada5hi <peter.placzek1996@gmail.com>
2023-01-12 14:55:57 +0100

    fix: extended type information + added README.md section

--- src/type.ts
+++ src/type.ts
@@ -8,9 +8,21 @@
 export type TypeMap = Record<string, string[] | TypeMetaInput>;
 
 export type TypeMeta = {
+    /**
+     * Source of the mime type and associated information.
+     */
     source?: string,
+    /**
+     * Extensions associated with the mime type.
+     */
     extensions: string[],
+    /**
+     * Charset of the mime type content.
+     */
     charset?: string,
+    /**
+     * Content of mime type is compressible?
+     */
     compressible?: boolean,
 };
 
