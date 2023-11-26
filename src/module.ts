5554a44e0a55fa2db2853c6f6738f3b184bba641
tada5hi <peter.placzek1996@gmail.com>
2023-11-26 15:25:04 +0100

    build: remove copyright file header

--- src/module.ts
+++ src/module.ts
@@ -1,10 +1,3 @@
-/*
- * Copyright (c) 2023-2023.
- * Author Peter Placzek (tada5hi)
- * For the full copyright and license information,
- * view the LICENSE file that was distributed with this source code.
- */
-
 import type { TypeMap, TypeMeta } from './type';
 import db from './db.json';
 
